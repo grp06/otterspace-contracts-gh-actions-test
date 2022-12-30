f609e5a706dfa8ccf92b260c24bd34d75aaeb020
grp06 <gpickett00@gmail.com>
2022-12-30 16:36:19 -0300

    testing new GH action

--- src/Raft.sol
+++ src/Raft.sol
@@ -48,7 +48,7 @@ contract Raft is
   }
 
   function getGeorge() public pure returns (string memory) {
-    return "gpra";
+    return "1";
   }
 
   function mint(address recipient, string memory uri)
