7a068f2dd5f94555f5bb8601b4b7801c0574c683
grp06 <gpickett00@gmail.com>
2023-01-20 09:12:57 -0300

    small update to raft

--- src/Raft.sol
+++ src/Raft.sol
@@ -48,7 +48,7 @@ contract Raft is
   }
 
   function getGeorge() public pure returns (string memory) {
-    return "11";
+    return "12";
   }
 
   function mint(address recipient, string memory uri)
